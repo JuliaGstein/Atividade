58ae4064d360e3a392ff1c38bab158fd196802a0
Your Name <julia_graziela@estudante.senai.sc.br>
2017-12-01 00:17:56 +0000

    criando paginna html e aplication

--- app/controllers/aeronaves_controller.rb
+++ app/controllers/aeronaves_controller.rb
@@ -69,6 +69,6 @@ class AeronavesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def aeronafe_params
-      params.require(:aeronafe).permit(:nome, :numerotripulantes, :tamanho, :numero, :chassi, :quantidadehorasvoo, :quantidademotor)
+      params.require(:aeronave).permit(:nome, :numerotripulantes, :tamanho, :numero, :chassi, :quantidadehorasvoo, :quantidademotor)
     end
 end
